bb3d3e85bc4ae56da1ad3576d9c48a24012dd0f6
Mathias Wagner <germannewsmaker@gmail.com>
2023-04-08 02:01:06 +0200

    Updated the App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
-import './App.css'
+import './App.sass'
+import './styles/balls.sass';
+import './styles/button.sass';
+import Home from "./pages/Home/Home.jsx";
+import {useState} from "react";
+import CodeCreator from "./pages/CodeCreator/CodeCreator.jsx";
 
 function App() {
-  return (
-    <div className="App">
+    const [currentPage, setCurrentPage] = useState("home");
 
-    </div>
-  )
+    return (
+        <div className="App">
+            {currentPage === "home" && <Home setCurrentPage={setCurrentPage}/>}
+            {currentPage === "qr" && <CodeCreator />}
+
+            <div className="ball-area">
+                <div className="ball ball-top" />
+                <div className="ball ball-left" />
+                <div className="ball ball-right" />
+            </div>
+        </div>
+    )
 }
 
 export default App
