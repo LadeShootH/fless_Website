a7a27b7ffd65362ce958ee3636d08705e7fb2899
Mathias Wagner <germannewsmaker@gmail.com>
2023-05-07 15:00:26 +0200

    Updated the App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import './App.sass'
-import './styles/balls.sass';
 import './styles/button.sass';
 import Home from "./pages/Home/Home.jsx";
 import {useState} from "react";
@@ -11,13 +10,7 @@ function App() {
     return (
         <div className="App">
             {currentPage === "home" && <Home setCurrentPage={setCurrentPage}/>}
-            {currentPage === "qr" && <CodeCreator />}
-
-            <div className="ball-area">
-                <div className="ball ball-top" />
-                <div className="ball ball-left" />
-                <div className="ball ball-right" />
-            </div>
+            {currentPage === "qr" && <CodeCreator setCurrentPage={setCurrentPage} />}
         </div>
     )
 }
