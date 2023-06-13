f073293d02e37e565227c7bffa805439bc95dda8
Mathias Wagner <germannewsmaker@gmail.com>
2023-06-13 22:56:58 +0200

    Created the projects.js (json)

---
+++ src/pages/Projects/projects.js
@@ -0,0 +1,9 @@
+import SheepstarImage from "./images/sheepstar.png";
+
+export default [
+  {
+    title: "Sheepstar",
+    description: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
+    image: SheepstarImage,
+  }
+]
