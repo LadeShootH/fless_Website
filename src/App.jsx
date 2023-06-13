6652ecce4d747687b10df9e80c19d53d36a87909
Mathias Wagner <germannewsmaker@gmail.com>
2023-06-13 22:58:34 +0200

    Added the Projects page to the App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './styles/button.sass';
 import Home from "./pages/Home/Home.jsx";
 import {useState} from "react";
 import CodeCreator from "./pages/CodeCreator/CodeCreator.jsx";
+import Projects from "./pages/Projects/Projects.jsx";
 
 function App() {
     const [currentPage, setCurrentPage] = useState("home");
@@ -11,6 +12,7 @@ function App() {
         <div className="App">
             {currentPage === "home" && <Home setCurrentPage={setCurrentPage}/>}
             {currentPage === "qr" && <CodeCreator setCurrentPage={setCurrentPage} />}
+            {currentPage === "projects" && <Projects setCurrentPage={setCurrentPage} />}
         </div>
     )
 }
