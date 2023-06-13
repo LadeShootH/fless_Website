2c963ca4b69e7821662c1e6577e203d33ae06866
Mathias Wagner <germannewsmaker@gmail.com>
2023-06-13 22:55:32 +0200

    Updated the Button.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,9 @@
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import "../styles/button.sass";
 
-export default ({text, icon, onClick}) => {
+export default ({text, icon, onClick, color}) => {
     return (
-        <button className="btn" onClick={onClick}>
+        <button className={"btn" + (color ? " color-" + color : "")} onClick={onClick}>
             {icon && <FontAwesomeIcon icon={icon} size="2x"/>}
             <h1>{text}</h1>
         </button>
