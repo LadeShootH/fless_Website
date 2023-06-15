dba95758f6136f66540b21e57d3006d079a929f8
Leon <68347090+LadeShootH@users.noreply.github.com>
2023-06-15 22:52:10 +0200

    Update projects.js
    Junge Martin du HS warum sagst du mir Fehler aber verbesserst nicht gleich so digga hä ob jetzt ich oder du das komma setzt rr

--- src/pages/Projects/projects.js
+++ src/pages/Projects/projects.js
@@ -5,7 +5,7 @@ export default [
     title: "STOW301",
     description: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
     image: SheepstarImage,
-  }
+  },
   {
     title: "Sheepstar",
     description: "...ist ein Chatbot für die Platform “Discord”. Zusammen mit meinem Freund Mathias habe ich diesen (keine Ahnung Verb). Meine Aufgabe war es hierbei unter anderem den Bot zu vermarkten bzw die Social Media Präsens zu verwalten.",
