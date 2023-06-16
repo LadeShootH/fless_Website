b5bec7c828270b24eb14e330237e99c4be36fbd9
Leon <68347090+LadeShootH@users.noreply.github.com>
2023-06-17 00:31:55 +0200

    Update projects.js
    Martin antwortet wieder nicht deswegen idk obs geht

--- src/pages/Projects/projects.js
+++ src/pages/Projects/projects.js
@@ -3,12 +3,12 @@ import SheepstarImage from "./images/sheepstar.png";
 export default [
       {
     title: "STOW301",
-    description: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
-    image: SheepstarImage,
+    description: "... ist ein privater Freiluft-Club in 91301 Forchheim. Ich war hier 1/2 Veranstaltern und war hauptsächlich für das Marketing zuständig.",
+    image: "https://i.imgur.com/rtwXhl1.png",
   },
   {
     title: "Sheepstar",
-    description: "...ist ein Chatbot für die Platform “Discord”. Zusammen mit meinem Freund Mathias habe ich diesen (keine Ahnung Verb). Meine Aufgabe war es hierbei unter anderem den Bot zu vermarkten bzw die Social Media Präsens zu verwalten.",
+    description: "... ist ein Chatbot für die Platform “Discord”. Zusammen mit meinem Freund Mathias habe ich diesen (keine Ahnung Verb). Meine Aufgabe war es hierbei unter anderem den Bot zu vermarkten bzw die Social Media Präsens zu verwalten.",
     image: SheepstarImage,
   }
 ]
