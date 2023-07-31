c157f066478c01a437c083dde63e97554f9dacfb
Mathias Wagner <germannewsmaker@gmail.com>
2023-07-31 18:52:15 +0200

    Added links to the projects.js

--- src/pages/Projects/projects.js
+++ src/pages/Projects/projects.js
@@ -1,14 +1,16 @@
 import SheepstarImage from "./images/sheepstar.png";
 
 export default [
-      {
-    title: "STOW301",
-    description: "... ist ein privater Freiluft-Club in 91301 Forchheim. Ich war hier 1/2 Veranstaltern und war hauptsächlich für das Marketing zuständig.",
-    image: "https://i.imgur.com/rtwXhl1.png",
-  },
-  {
-    title: "Sheepstar",
-    description: "... ist ein Chatbot für die Plattform “Discord”. Zusammen mit meinem Freund Mathias habe ich diesen aufgebaut. Meine Aufgabe war es hierbei unter anderem den Bot zu vermarkten bzw die Social Media Präsenz zu verwalten.",
-    image: SheepstarImage,
-  }
+    {
+        title: "STOW301",
+        description: "... ist ein privater Freiluft-Club in 91301 Forchheim. Ich war hier 1/2 Veranstaltern und war hauptsächlich für das Marketing zuständig.",
+        image: "https://i.imgur.com/rtwXhl1.png",
+        link: "#"
+    },
+    {
+        title: "Sheepstar",
+        description: "... ist ein Chatbot für die Plattform “Discord”. Zusammen mit meinem Freund Mathias habe ich diesen aufgebaut. Meine Aufgabe war es hierbei unter anderem den Bot zu vermarkten bzw die Social Media Präsenz zu verwalten.",
+        image: SheepstarImage,
+        link: "#"
+    }
 ]
