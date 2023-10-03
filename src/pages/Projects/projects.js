53cd64bc54eca2de7cc98c343fcc2c18438f1a8f
Leon <68347090+LadeShootH@users.noreply.github.com>
2023-10-03 15:39:55 +0200

    Update projects.js
    Added st:dio

--- src/pages/Projects/projects.js
+++ src/pages/Projects/projects.js
@@ -2,6 +2,12 @@ import SheepstarImage from "./images/sheepstar.png";
 
 export default [
     {
+        title: "st:dio",
+        description: "... ist eine Branding Agency, an welcher ich als Grafik-Designer und Video-Editor für Kunden und die Agentur selbst arbeite.",
+        image: "https://scontent-fra5-2.cdninstagram.com/v/t51.2885-19/366465238_967629864286998_5864536818245601229_n.jpg?stp=dst-jpg_s320x320&_nc_ht=scontent-fra5-2.cdninstagram.com&_nc_cat=106&_nc_ohc=gL1PBPMo000AX8uVa1R&edm=AOQ1c0wBAAAA&ccb=7-5&oh=00_AfD543qWrR_zOoU5Fa3FO4_N8Ig8KW81uZb4BQwsbv2akg&oe=6521896A&_nc_sid=8b3546",
+        link: "https://stdio.digital"
+    },
+    {
         title: "Hey, Zukunft!",
         description: "... ist ein Blog über Themen wie Innovationen, Start-Ups und unsere Zukunft, den ich infolge meines Technik-Journalismus-Studiums 2023 startete.",
         image: "https://blog.fless.me/wp-content/uploads/2023/08/IMG_0447.png",
