8d698c432107278579732274863bb32c4a568273
Leon <68347090+LadeShootH@users.noreply.github.com>
2024-05-24 13:41:36 +0200

    Update projects.js
    Added LEON301
    Updated all descriptions

--- src/pages/Projects/projects.js
+++ src/pages/Projects/projects.js
@@ -3,37 +3,43 @@ import SheepstarImage from "./images/sheepstar.png";
 export default [
      {
         title: "STOW301",
-        description: "... ist ein Kulturverein in 91301 Forchheim, mit dem Zweck, lokale Künstler zu fördern. Ich bin der 2. Vorstandsvorsitzende und hauptsächlich für das Marketing zuständig.",
+        description: "... ist ein Kulturverein in 91301 Forchheim mit dem Ziel, lokale Künstler zu fördern und die Jugendkultur zu stärken. Ich bin 2. Vorstand und hauptsächlich für das Marketing zuständig.",
         image: "https://i.imgur.com/25dHmqT.png",
         link: "https://www.behance.net/gallery/145653905/Stow301-Social-Media-Posts"
     },
+     {
+        title: "LEON301",
+        description: "... ist ein Musikerprofil von mir, auf dem ich KI-Songs veröffentliche.",
+        image: "https://i.scdn.co/image/ab67616d00001e023676102cf8a48f25bfa9cda6",
+        link: "https://open.spotify.com/intl-de/artist/5hFm781ZE8EQ0pg2lrFSWa?si=eTbBg8duQDeTOst4ViJRFA"
+    },
     {
         title: "st:dio",
-        description: "... ist eine Branding Agency, an welcher ich freiberuflich als Grafik-Designer und Video-Editor für Kunden und die Agentur mitarbeite.",
+        description: "... ist eine Branding-Agentur, in der ich freiberuflich als Grafik-Designer und Video-Editor für Kunden und die Agentur tätig bin.",
         image: "https://i.imgur.com/JJdqVKa.png",
         link: "https://stdio.digital/"
     },
     {
         title: "Hey, Zukunft!",
-        description: "... ist ein Blog über Themen wie Innovationen, Start-Ups und unsere Zukunft, den ich infolge meines Technikjournalismus-Studiums 2023 startete.",
+        description: "... ist ein Blog über Innovationen, Start-Ups und unsere Zukunft, den ich im infolge an mein Technikjournalismus-Studium 2023 gestartet habe.",
         image: "https://blog.fless.me/wp-content/uploads/2023/08/IMG_0447.png",
         link: "https://blog.fless.me/"
     },
     {
         title: "Sheepstar",
-        description: "... ist ein Chatbot für die Plattform “Discord”. Zusammen mit meinem Freund Mathias habe ich diesen aufgebaut. Meine Aufgabe war es hierbei unter anderem den Bot zu vermarkten und die Social Media Präsenz zu verwalten.",
+        description: "... ist ein Chatbot für die Plattform “Discord”. Zusammen mit meinem Freund Mathias habe ich ihn entwickelt. Meine Aufgabe war es, den Bot zu vermarkten und die Social-Media-Präsenz zu betreuen.",
         image: SheepstarImage,
         link: "https://www.behance.net/gallery/176953699/Sheepstar-Discord-Bot"
     },
     {
         title: "LeynoxFX",
-        description: "... ist der Name, den ich online als Grafik-Designer nutze. Angefangen habe ich mit Social Media Bannern, heute erstelle ich vor allem Social Media Posts, Werbebanner und Cover-Arts.",
+        description: "... ist der Name, unter dem ich als Grafik-Designer tätig bin. Angefangen habe ich mit Social Media Bannern, heute erstelle ich hauptsächlich Social Media Posts, Werbebanner und Cover Art.",
         image: "https://mir-s3-cdn-cf.behance.net/user/230/c283111052894635.612199e4e897f.png",
         link: "https://www.behance.net/leynoxfx"
     },
         {
         title: "LeynoxFX - Fiverr",
-        description: "Auf dieser Plattform arbeite ich bereits seit 2020 als freiberuflicher Creator für Kunden jeglicher Art. Mein Schwerpunkt liegt dabei auf der Erstellung ansprechender Digitalmedien, darunter Instagram-Posts, Songcover und Video-Thumbnails.",
+        description: "Auf dieser Plattform arbeite ich seit 2020 als freiberuflicher Kreativer für Kunden aller Art. Mein Schwerpunkt liegt auf der Erstellung von ansprechenden digitalen Medien wie Instagram-Posts, Songcover und Video-Thumbnails.",
         image: "https://seeklogo.com/images/F/fiverr-new-2020-logo-354E8A08FD-seeklogo.com.png",
         link: "https://de.fiverr.com/leynoxartz"
     }
