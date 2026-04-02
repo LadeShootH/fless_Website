6e26ea092178b6346f93ebea46a022a74ba69499
Leon <68347090+LadeShootH@users.noreply.github.com>
2026-04-02 19:43:03 +0200

    Update projects.js
    Added Project "Junge Liste Forchheim"

--- src/pages/Projects/projects.js
+++ src/pages/Projects/projects.js
@@ -7,18 +7,24 @@ export default [
         image: "https://i.imgur.com/25dHmqT.png",
         link: "https://stow301.de"
     },
+          {
+        title: "Junge Liste Forchheim",
+        description: "...ist eine Wählergemeinschaft, die 2026 zur Kommunalwahl in der Stadt Forchheim antrat. Ich selbst belegte Listenplatz 2 und fokussierte mich auf die Themen Kultur und Jugendpartizipation.",
+        image: "https://jungeliste-fo.de/wp-content/uploads/2025/12/cropped-JungeListe_Logo-scaled-1-180x180.jpg",
+        link: "https://jungeliste-fo.de"
+    },
          {
         title: "LeynoxFX",
         description: "... ist der Name, unter dem ich als Grafik-Designer tätig bin. Angefangen habe ich mit Social Media Bannern, heute erstelle ich hauptsächlich Social Media Posts, Werbebanner und Cover Art.",
         image: "https://mir-s3-cdn-cf.behance.net/user/230/c283111052894635.612199e4e897f.png",
         link: "https://www.behance.net/leynoxfx"
     },
-         {
-        title: "st:dio",
-        description: "... ist eine Branding-Agentur, in der ich freiberuflich als Grafik-Designer und Video-Editor für Kunden und die Agentur tätig bin.",
-        image: "https://i.imgur.com/JJdqVKa.png",
-        link: "https://stdio.digital/"
-    },
+//         {
+//        title: "st:dio",
+//        description: "... ist eine Branding-Agentur, in der ich freiberuflich als Grafik-Designer und Video-Editor für Kunden und die Agentur tätig bin.",
+//        image: "https://i.imgur.com/JJdqVKa.png",
+//        link: "https://stdio.digital/"
+//    },
 //     {
 //        title: "LEON301",
 //        description: "... ist ein Musikerprofil von mir, auf dem ich KI-Songs veröffentliche.",
